c9347c3ea76a236a0c7b3b8468ac4a224f880f9e
xSparky911x <donsopher@hotmail.com>
2024-10-14 11:15:09 -0600

    added login and logout events

--- main.lua
+++ main.lua
@@ -26,11 +26,17 @@ Config.hooks.PLAYER_EVENT_ON_GROUP_CHAT = nil
 -- Webhook to send OnGuildChat events
 Config.hooks.PLAYER_EVENT_ON_GUILD_CHAT = nil
 
+-- Webhook to send Login and Logout events
+Config.hooks.PLAYER_EVENT_ON_LOGIN = nil
+Config.hooks.PLAYER_EVENT_ON_LOGOUT = nil
+
 -- Feature Flag for enabiling each event
 Config.eventOn.PLAYER_EVENT_ON_CHAT = true
 Config.eventOn.PLAYER_EVENT_ON_WHISPER = true
 Config.eventOn.PLAYER_EVENT_ON_GROUP_CHAT = true
 Config.eventOn.PLAYER_EVENT_ON_GUILD_CHAT = true
+Config.eventOn.PLAYER_EVENT_ON_LOGIN = true
+Config.eventOn.PLAYER_EVENT_ON_LOGOUT = true
 
 --[[
 
@@ -45,6 +51,7 @@ local PLAYER_EVENT_ON_GUILD_CHAT = 21
 
 -- Misc
 local PLAYER_EVENT_ON_LOGIN = 3
+local PLAYER_EVENT_ON_LOGOUT = 4
 
 --[[
 
@@ -70,6 +77,28 @@ end
     Events Section -- DO NOT TOUCH!!
 
 ]]--
+
+-- OnLogin
+local function OnLogin(event, player)
+    if Config.eventOn.PLAYER_EVENT_ON_LOGIN then
+        local accountid = player:GetAccountId()
+        local accountname = player:GetAccountName()
+        local name = player:GetName()
+        local guid = player:GetGUIDLow()
+        sendToDiscord("PLAYER_EVENT_ON_LOGIN", '__Logged on__ -> **['..accountid..'] '..accountname..' - ['..guid..'] '..name..'**')
+    end
+end
+
+-- OnLogout
+local function OnLogout(event, player)
+    if Config.eventOn.PLAYER_EVENT_ON_LOGOUT then
+        local name = player:GetName()
+        local guid = player:GetGUIDLow()
+        local accountid = player:GetAccountId()
+        local accountname = player:GetAccountName()
+        sendToDiscord("PLAYER_EVENT_ON_LOGOUT", '__Logged out__ -> **['..accountid..'] '..accountname..' - ['..guid..'] '..name..'**')
+    end
+end
 
 -- OnChat
 local function OnChat(event, player, msg, Type, lang)
@@ -129,6 +158,10 @@ RegisterPlayerEvent(PLAYER_EVENT_ON_WHISPER, OnWhisperChat)
 RegisterPlayerEvent(PLAYER_EVENT_ON_GROUP_CHAT, OnGroupChat)
 -- OnGuildChat
 RegisterPlayerEvent(PLAYER_EVENT_ON_GUILD_CHAT, OnGuildChat)
+-- OnLogin
+RegisterPlayerEvent(PLAYER_EVENT_ON_LOGIN, OnLogin)
+-- OnLogout
+RegisterPlayerEvent(PLAYER_EVENT_ON_LOGOUT, OnLogout)
 
 
 --[[
